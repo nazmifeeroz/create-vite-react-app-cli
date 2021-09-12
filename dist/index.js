196a259d44716e556eba3a81c42aa1116ca546f5
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2021-09-12 20:16:00 +0800

    :wrench: update bin name and package choice

--- dist/index.js
+++ dist/index.js
@@ -26,23 +26,22 @@ const inquirer = __importStar(require("inquirer"));
 const chalk_1 = require("chalk");
 const shell = __importStar(require("shelljs"));
 const template = __importStar(require("./utils/template"));
-const CHOICES = fs.readdirSync(path.join(__dirname, "templates"));
 const QUESTIONS = [
-    {
-        name: "template",
-        type: "list",
-        message: "What template would you like to use?",
-        choices: CHOICES,
-    },
     {
         name: "name",
         type: "input",
         message: "Please input a new project name:",
     },
+    {
+        name: "packageManager",
+        type: "list",
+        message: "Which package manager do you wish to use:",
+        choices: ["yarn", "npm"],
+    },
 ];
 const CURR_DIR = process.cwd();
 inquirer.prompt(QUESTIONS).then((answers) => {
-    const projectChoice = answers["template"];
+    const projectChoice = "react-template";
     const projectName = answers["name"];
     const templatePath = path.join(__dirname, "templates", projectChoice);
     const tartgetPath = path.join(CURR_DIR, projectName);
@@ -51,12 +50,13 @@ inquirer.prompt(QUESTIONS).then((answers) => {
         templateName: projectChoice,
         templatePath,
         tartgetPath,
+        packageManagerChoice: answers["packageManagerChoice"],
     };
     if (!createProject(options.tartgetPath)) {
         return;
     }
     createDirectoryContents(options.templatePath, options.projectName);
-    // postProcess(options);
+    postProcess(options);
 });
 function createProject(projectPath) {
     if (fs.existsSync(projectPath)) {
@@ -98,85 +98,10 @@ function postProcess(options) {
     const isNode = fs.existsSync(path.join(options.templatePath, "package.json"));
     if (isNode) {
         shell.cd(options.tartgetPath);
-        const result = shell.exec("npm install");
+        const result = shell.exec(`${options.packageManagerChoice} ${options.packageManagerChoice === "npm" ? "install" : ""}`);
         if (result.code !== 0) {
             return false;
         }
     }
     return true;
 }
-// #!/usr/bin/env node
-// import * as fs from "fs";
-// import * as path from "path";
-// import * as inquirer from "inquirer";
-// import { red } from "chalk";
-// import * as shell from "shelljs";
-// export interface CliOptions {
-//   projectName: string;
-//   targetPath: string;
-//   templatePath: string;
-// }
-// const CURR_DIR = process.cwd();
-// const QUESTIONS = [
-//   {
-//     name: "name",
-//     type: "input",
-//     message: "New project name?",
-//   },
-// ];
-// function createProject(projectPath: string) {
-//   if (fs.existsSync(projectPath)) {
-//     console.log(
-//       red(`Folder ${projectPath} exists. Delete or use another name.`)
-//     );
-//     return false;
-//   }
-//   fs.mkdirSync(projectPath);
-//   return true;
-// }
-// function createDirectoryContents(
-//   templatePath: string,
-//   targetPath: string,
-//   projectName: string
-// ) {
-//   const SKIP_FILES = ["node_modules"];
-//   const filesToCreate = fs.readdirSync(templatePath);
-//   filesToCreate.forEach((file) => {
-//     if (SKIP_FILES.indexOf(file) > -1) return;
-//     const origFilePath = path.join(targetPath, file);
-//     const stats = fs.statSync(origFilePath);
-//     if (stats.isFile()) {
-//       let contents = fs.readFileSync(origFilePath, "utf8");
-//       const writePath = path.join(targetPath, file);
-//       fs.writeFileSync(writePath, contents, "utf8");
-//     } else if (stats.isDirectory()) {
-//       fs.mkdirSync(path.join(targetPath, file));
-//       createDirectoryContents(
-//         targetPath,
-//         targetPath,
-//         path.join(projectName, file)
-//       );
-//     }
-//   });
-// }
-// inquirer.prompt(QUESTIONS).then((answers) => {
-//   const projectName = answers["name"];
-//   const targetPath = path.join(CURR_DIR, projectName);
-//   const templatePath = path.join(CURR_DIR, "src/templates");
-//   const options: CliOptions = {
-//     projectName,
-//     targetPath,
-//     templatePath,
-//   };
-//   if (!createProject(options.targetPath)) return;
-//   shell.exec(
-//     `git clone https://github.com/nazmifeeroz/vite-reactts-eslint-prettier ${options.templatePath}`,
-//     () => {
-//       createDirectoryContents(
-//         options.templatePath,
-//         options.targetPath,
-//         options.projectName
-//       );
-//     }
-//   );
-// });
--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,17 @@ import { red } from "chalk";
 import * as shell from "shelljs";
 import * as template from "./utils/template";
 
-const CHOICES = fs.readdirSync(path.join(__dirname, "templates"));
 const QUESTIONS = [
-  {
-    name: "template",
-    type: "list",
-    message: "What template would you like to use?",
-    choices: CHOICES,
-  },
   {
     name: "name",
     type: "input",
     message: "Please input a new project name:",
+  },
+  {
+    name: "packageManager",
+    type: "list",
+    message: "Which package manager do you wish to use:",
+    choices: ["yarn", "npm"],
   },
 ];
 
@@ -27,12 +26,13 @@ export interface CliOptions {
   templateName: string;
   templatePath: string;
   tartgetPath: string;
+  packageManagerChoice: string;
 }
 
 const CURR_DIR = process.cwd();
 
 inquirer.prompt(QUESTIONS).then((answers) => {
-  const projectChoice = answers["template"];
+  const projectChoice = "react-template";
   const projectName = answers["name"];
   const templatePath = path.join(__dirname, "templates", projectChoice);
   const tartgetPath = path.join(CURR_DIR, projectName);
@@ -42,6 +42,7 @@ inquirer.prompt(QUESTIONS).then((answers) => {
     templateName: projectChoice,
     templatePath,
     tartgetPath,
+    packageManagerChoice: answers["packageManagerChoice"],
   };
 
   if (!createProject(options.tartgetPath)) {
@@ -50,7 +51,7 @@ inquirer.prompt(QUESTIONS).then((answers) => {
 
   createDirectoryContents(options.templatePath, options.projectName);
 
-  // postProcess(options);
+  postProcess(options);
 });
 
 function createProject(projectPath: string) {
@@ -103,7 +104,11 @@ function postProcess(options: CliOptions) {
   const isNode = fs.existsSync(path.join(options.templatePath, "package.json"));
   if (isNode) {
     shell.cd(options.tartgetPath);
-    const result = shell.exec("npm install");
+    const result = shell.exec(
+      `${options.packageManagerChoice} ${
+        options.packageManagerChoice === "npm" ? "install" : ""
+      }`
+    );
     if (result.code !== 0) {
       return false;
     }
@@ -111,95 +116,3 @@ function postProcess(options: CliOptions) {
 
   return true;
 }
-
-// #!/usr/bin/env node
-
-// import * as fs from "fs";
-// import * as path from "path";
-// import * as inquirer from "inquirer";
-// import { red } from "chalk";
-// import * as shell from "shelljs";
-
-// export interface CliOptions {
-//   projectName: string;
-//   targetPath: string;
-//   templatePath: string;
-// }
-
-// const CURR_DIR = process.cwd();
-
-// const QUESTIONS = [
-//   {
-//     name: "name",
-//     type: "input",
-//     message: "New project name?",
-//   },
-// ];
-
-// function createProject(projectPath: string) {
-//   if (fs.existsSync(projectPath)) {
-//     console.log(
-//       red(`Folder ${projectPath} exists. Delete or use another name.`)
-//     );
-//     return false;
-//   }
-//   fs.mkdirSync(projectPath);
-
-//   return true;
-// }
-
-// function createDirectoryContents(
-//   templatePath: string,
-//   targetPath: string,
-//   projectName: string
-// ) {
-//   const SKIP_FILES = ["node_modules"];
-
-//   const filesToCreate = fs.readdirSync(templatePath);
-
-//   filesToCreate.forEach((file) => {
-//     if (SKIP_FILES.indexOf(file) > -1) return;
-
-//     const origFilePath = path.join(targetPath, file);
-
-//     const stats = fs.statSync(origFilePath);
-
-//     if (stats.isFile()) {
-//       let contents = fs.readFileSync(origFilePath, "utf8");
-//       const writePath = path.join(targetPath, file);
-//       fs.writeFileSync(writePath, contents, "utf8");
-//     } else if (stats.isDirectory()) {
-//       fs.mkdirSync(path.join(targetPath, file));
-//       createDirectoryContents(
-//         targetPath,
-//         targetPath,
-//         path.join(projectName, file)
-//       );
-//     }
-//   });
-// }
-
-// inquirer.prompt(QUESTIONS).then((answers) => {
-//   const projectName = answers["name"];
-//   const targetPath = path.join(CURR_DIR, projectName);
-//   const templatePath = path.join(CURR_DIR, "src/templates");
-
-//   const options: CliOptions = {
-//     projectName,
-//     targetPath,
-//     templatePath,
-//   };
-
-//   if (!createProject(options.targetPath)) return;
-
-//   shell.exec(
-//     `git clone https://github.com/nazmifeeroz/vite-reactts-eslint-prettier ${options.templatePath}`,
-//     () => {
-//       createDirectoryContents(
-//         options.templatePath,
-//         options.targetPath,
-//         options.projectName
-//       );
-//     }
-//   );
-// });
